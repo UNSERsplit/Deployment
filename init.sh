a93cc6e03e33593daca49eaac41379bf93f99fc4
Noah <82201000+Nopro263@users.noreply.github.com>
2025-01-24 12:42:14 +0100

    added init

--- init.sh
+++ init.sh
@@ -4,11 +4,14 @@ source .env
 
 DIR="/var/www/split"
 CONF="/etc/nginx/sites-available/split.conf"
+TARGET="/etc/nginx/sites-enabled/split.conf"
 TMP="/tmp/ci"
 
 rm -rf ${TMP}
 git clone https://${ACCESS_TOKEN}@github.com/UNSERsplit/Deployment ${TMP}
-
+cp ${TMP}/nginx.conf ${CONF}
+ln -s ${CONF} ${TARGET}
+nginx -s reload
 
 rm -rf ${DIR}
 git clone https://${ACCESS_TOKEN}@github.com/UNSERsplit/Backend ${DIR}
